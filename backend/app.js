9860371fec181d417cbe88fc7363380afc2dc493
Saurabh <saurabhnegi0007@gmail.com>
2023-01-13 01:20:28 +0530

    added home route using express router

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
+const todoRoutes = require("./routes/todoRoutes.js");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.use("/", todoRoutes);
 
 module.exports = app;
 
